7691ea88881c3bf7be7f5f928941541e078896e3
DylanParenzee <dylanparenzee@hotmail.com>
2023-02-24 21:45:56 +1100

    completed questions

--- sandbox.py
+++ sandbox.py
@@ -7,3 +7,18 @@
 num_of_cats = 10 
 
 print(num_of_cats * 2)
+
+# as part of colt steel's python course test 
+
+# 24.02.23
+# Define a variable named city and set it equal to any string
+city = "Sydney"
+
+# Define a variable named price and set it equal to any float
+price = 23.99
+
+# Define a variable named high_score and set it equal to any int
+high_score = 2983729
+
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
