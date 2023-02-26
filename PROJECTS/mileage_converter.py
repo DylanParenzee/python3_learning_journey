3c8de8396c01a7efae5d9db6c3e3b8f66f1a795a
DylanParenzee <dylanparenzee@hotmail.com>
2023-02-26 11:29:05 +1100

    updated

--- PROJECTS/mileage_converter.py
+++ PROJECTS/mileage_converter.py
@@ -6,4 +6,15 @@ print("How many Miles did you run today ?")
 miles = input()
 kms = float(miles) * 1.6909
 
-print(f"You ran {miles} or {kms} kilometers")
+
+print(f"You ran {miles} or {round(kms,2)} kilometers")
+
+
+#input() 
+
+#gets user input and saves to variable 
+
+
+#round()
+
+#rounds the float you supply to the amout of decimal places you select.
