cd6bae087a2005de4236c7db306eec1f89f00fb9
DylanParenzee <dylanparenzee@hotmail.com>
2023-02-27 19:46:01 +1100

    shocased other concat styles

---
+++ PROJECTS/madlibs.py
@@ -0,0 +1,10 @@
+#small madlibs project by kylie ying to practice string concatenation
+
+
+# A couple ways to concat -- gonna go with the f string -- much easier and more what im used to.
+
+youtuber = "Dylan"
+
+print('please subsribe to ' + youtuber)
+print('please subsribe to {}'.format(youtuber))
+print(f'please subsribe to {youtuber}')
