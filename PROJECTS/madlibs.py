febb51657116d74d3dc7779af4b2930c916c443c
DylanParenzee <dylanparenzee@hotmail.com>
2023-02-28 19:57:36 +1100

    update to madlibs.py

--- PROJECTS/madlibs.py
+++ PROJECTS/madlibs.py
@@ -16,7 +16,7 @@ verb2 = input("Verb2: ")
 famous_person = input("Your favourite famous person: ")
 
 madlib = f"computer programming is so {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}, what a good role model!"
 
 
 print(madlib)
