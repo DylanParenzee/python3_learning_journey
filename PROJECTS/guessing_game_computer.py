787908e56f8a5ffb4ee2422f0c09e41aac907db6
DylanParenzee <dylanparenzee@hotmail.com>
2023-03-02 17:30:19 +1100

    completed project, made guessing game more dynamic by allowing user to choose what number they would like to play to instead of hard coding in

--- PROJECTS/guessing_game_computer.py
+++ PROJECTS/guessing_game_computer.py
@@ -2,8 +2,18 @@ import random
 
 
 
-def guess(x):
-    imput("Lets play a guessing game, whats the max number?")
+def guess():
+    x = int(input('whats the max number to play to?'))
+    rand_num = random.randint(1, x)
+    guess = 0 
+    while(guess != rand_num) :
+        guess = int(input(f"Lets play a guessing game, guess a number between 1 and {x} "))
+        if guess < rand_num:
+            print('whoops too low, guess again')
+        elif guess > rand_num:
+            print('ohhh too high! guess again')
+    print(f'You got it! good guess, the number was {rand_num}')
+            
 
 
-
+guess()
