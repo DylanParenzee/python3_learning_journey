84e6fc8278d8a2f9c00447ead855654bb84cbca6
DylanParenzee <dylanparenzee@hotmail.com>
2023-03-07 19:47:53 +1100

    completed quiz

--- sandbox.py
+++ sandbox.py
@@ -89,3 +89,23 @@ if choice == 7:
     print('lucky')
 else:
     print('unlucky sorr try again ')
+
+
+    #07.03.23
+
+#Number is Odd
+#You will be provided with a random number in a variable called num .
+
+#Use a conditional statement to check if the number is odd. If num  is odd, print "odd". Otherwise print "even". 
+
+#Hint: use modulus %  to figure out if the number is odd!
+
+
+from random import randint
+num = randint(1, 1000) 
+
+
+if num % 2 != 0:
+    print('odd')
+else:
+    print('even')
