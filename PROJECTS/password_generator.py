9396c7db9a71d8e84bf3ea0fba03f55580ab883b
DylanParenzee <dylanparenzee@hotmail.com>
2023-03-11 19:46:48 +1100

    added foundations

--- PROJECTS/password_generator.py
+++ PROJECTS/password_generator.py
@@ -1,2 +1,8 @@
 # goal is to generate a random 10 character password utilising upper and lowercase charcter, symbols and numbers. 
 
+import random 
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+symbols = '!@#$%^&*()_+?./\'
+numbers = '0123456789'
