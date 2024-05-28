377d49ed6ee3157daec9ce00ab0d081f084e79dd
DylanParenzee <dylanparenzee@hotmail.com>
2024-05-29 05:03:03 +1000

    day one of angela yus course

--- PROJECTS/band_name_generator.py
+++ PROJECTS/band_name_generator.py
@@ -1,2 +1,8 @@
-print('Hello')
+# messing around with input function
 
+user_city = input("What city do you live in?")
+user_pet = input('what is your first pet\'s name')
+band_name = f'{user_city.capitalize()} - {user_pet.capitalize()}'
+
+
+print( f" Your band name is {band_name}, nice" )
