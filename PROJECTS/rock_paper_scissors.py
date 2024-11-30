09a1d62bffacdfe22795402e93c49b3daf6fc2a4
DylanParenzee <dylanparenzee@hotmail.com>
2024-11-30 17:16:35 +1100

    proto -- so ugly so simple so lame, needs more work, will have to come back later

--- PROJECTS/rock_paper_scissors.py
+++ PROJECTS/rock_paper_scissors.py
@@ -1,1 +1,27 @@
 # create a rock paper scissors guessing game for 2 players from terminal
+ 
+ 
+ 
+
+
+def get_inputs():
+   player_1_input = input(f"player 1's' choice is ")
+   player_2_input = input(f"player 2's choice is ")
+   if player_1_input == "rock" and player_2_input == "paper":
+    print("Winner = Player 2! Paper beats rock")
+   elif player_1_input == "rock" and player_2_input == 'scissors':
+    print(f'Winner = Player 1! rock beats scissors')
+   elif player_1_input == "scissors" and player_2_input == 'rock':
+    print(f'Winner = Player 2! rock beats scissors')
+   elif player_1_input == "scissors" and player_2_input == 'paper':
+    print(f'Winner = Player 1! scissors beats paper')
+   elif player_1_input == "paper" and player_2_input == 'rock':
+    print(f'Winner = Player 1! paper beats rock')
+   elif player_1_input == "paper" and player_2_input == 'scissors':
+    print(f'Winner = Player 2! scissors beats paper')
+   else: print(f'its a draw!')
+
+game_continue = input('continue game ? y or n ')
+
+
+get_inputs()
