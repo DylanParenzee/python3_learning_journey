2b806c557cfabf881bdc08ad75b6f5458c3f96cd
DylanParenzee <dylanparenzee@hotmail.com>
2025-03-20 09:36:48 +1100

    hello

--- PROJECTS/band_name_generator.py
+++ PROJECTS/band_name_generator.py
@@ -1,7 +1,7 @@
 # messing around with input function
 
-user_city = input("What city do you live in?")
-user_pet = input('what is your first pet\'s name')
+user_city = input("What city do you live in? ")
+user_pet = input('what is your first pet\'s name ')
 band_name = f'{user_city.capitalize()} - {user_pet.capitalize()}'
 
 
