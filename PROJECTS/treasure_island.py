177a1c6c7fbe3ac16020e0d27f29a29cb0bad33d
DylanParenzee <dylanparenzee@hotmail.com>
2025-04-03 10:22:13 +1100

    skeleton of whats to come

---
+++ PROJECTS/treasure_island.py
@@ -0,0 +1,25 @@
+#This file will include the Treasue Island interactive game project and the learnings for it. 
+
+
+#conditinal logic and flow control 
+
+
+
+
+
+
+def start_game():
+   want_to_play = input("Helo would you like to play a game    y or n ? ")
+   if want_to_play == "y":
+      print("Awesome, Lets play treasure island!")
+      game_logic()
+   else:
+    print("No problem, have a good day!")
+
+
+
+
+
+
+def game_logic():
+  return none 
