d4cbc62bbe932af6e10b0b06aa1c1b499055deac
DylanParenzee <dylanparenzee@hotmail.com>
2025-04-03 12:59:26 +1100

    exercise completed

--- PROJECTS/treasure_island.py
+++ PROJECTS/treasure_island.py
@@ -3,10 +3,22 @@
 
 #conditinal logic and flow control 
 
+#exercise
+
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("underweight")
+elif (bmi > 18.5 ) and (bmi< 25):
+    print('normal weight')
+else: 
+    print('overweight')
 
 
-
-
+#game 
 
 def start_game():
    want_to_play = input("Helo would you like to play a game    y or n ? ")
